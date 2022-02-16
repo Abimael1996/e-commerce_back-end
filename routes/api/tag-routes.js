51985df48699fd9a0a563ca3e77f0a9c6afd7d14
abi <abimael.monarrez58@gmail.com>
2022-02-16 16:57:36 -0700

    finish POST request for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,7 +38,7 @@ router.get('/:id', async (req, res) => {
       ]
     });
     if(!tagData) {
-      res.status(400).json("Tag doesn0t exist")
+      res.status(400).json("Tag doesn't exist")
     } else {
       res.status(200).json(tagData)
     }
@@ -47,11 +47,19 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    if(req.body.tag_name) {
+      const tagData = await Tag.create(req.body);
+      res.status(200).json(`Tag ${tagData.tag_name} was added to the database`)
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
 });
 
