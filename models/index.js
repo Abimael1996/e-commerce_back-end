0e49374ff1412d989a378a127be3a034b0ef3c9c
abi <abimael.monarrez58@gmail.com>
2022-02-16 11:36:54 -0700

    add foreing keys so that they don't get created automatically

--- models/index.js
+++ models/index.js
@@ -5,7 +5,9 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+});
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
@@ -16,14 +18,16 @@ Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     unique: false
-  }
+  },
+  foreignKey: "product_id"
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
     unique: false
-  }
+  },
+  foreignKey: "tag_id"
 });
 module.exports = {
   Product,
