cd2f548fe4b0d62bd260f94bd99e0caf4ad7d95c
abi <abimael.monarrez58@gmail.com>
2022-02-16 14:50:11 -0700

    finish DELETE request for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -131,8 +131,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!deletedProduct) {
+      res.status(400).json("The product you are tying to delete doesn't exist")
+    } else {
+      res.status(200).json(deletedProduct);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
