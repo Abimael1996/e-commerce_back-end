c922b4a97c4e2976ae4a8a056d9ad049b29aefea
abi <abimael.monarrez58@gmail.com>
2022-02-16 19:08:14 -0700

    finish POST request of categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,8 +45,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+      res.status(200).json(`${newCategory.category_name} category has been added to the database`)
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
