5ee044e6e41f3543c43169b6fc1f7f961f0f574d
abi <abimael.monarrez58@gmail.com>
2022-02-16 18:30:36 -0700

    finish GET request for all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, Tag } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoriesData = await Category.findAll({
+      include: [
+        {
+          model: Product
+        }
+      ]
+    });
+    if (!categoriesData) {
+      res.status(404).json("No categories found")
+    } else {
+      res.status(200).json(categoriesData)
+    }
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 router.get('/:id', (req, res) => {
